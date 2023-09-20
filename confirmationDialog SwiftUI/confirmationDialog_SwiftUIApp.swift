dd75a14913ba1b7ba10510683baabcdc47361feb
mk812y <mk812y@gmail.com>
2023-09-20 19:40:39 +0200

    Initial Commit

---
+++ confirmationDialog SwiftUI/confirmationDialog_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  confirmationDialog_SwiftUIApp.swift
+//  confirmationDialog SwiftUI
+//
+//  Created by Михаил Куприянов on 20.9.23..
+//
+
+import SwiftUI
+
+@main
+struct confirmationDialog_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
