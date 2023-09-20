376e0a01cde0297ff395a8a3917371bf710c992b
mk812y <mk812y@gmail.com>
2023-09-20 19:56:08 +0200

    ок

--- confirmationDialog SwiftUI/ContentView.swift
+++ confirmationDialog SwiftUI/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPresented = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//        Button("Show Alert") { //Alert
+        Button("Show Action Sheet") {
+            isPresented.toggle()
         }
-        .padding()
+        .buttonStyle(.borderedProminent)
+//        .alert("The button was tapped", //Alert
+//            isPresented: $isPresented,
+//            actions: {}) {
+//            Text("Please confirm")
+//        }
+        .confirmationDialog(
+            "Delete data?",//по умолчанию не отображается
+            isPresented: $isPresented,
+            titleVisibility: .visible//отображает скрытый по умолчанию "Delete data?"
+        ) {
+            Button("Yes, Delete", role: .destructive) {}//поменяли дефолтный цвет кнопки на красный
+            Button("Delete but keep setting") {}
+            Button("No, please", role: .cancel) {}//поменяли текст дефолтной кнопки Cancel
+        } message: {//message необязательный
+            Text("Please choose one option")
+        }
+        
     }
 }
 
